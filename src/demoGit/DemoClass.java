330d4381a2175cb569f5d9734ed366ac008609b6
Mohit Kaushik <mkaushik@eshopworld.com>
2021-01-10 01:27:50 +0000

    First Commit.....

---
+++ src/demoGit/DemoClass.java
@@ -0,0 +1,18 @@
+package demoGit;
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int c = addTwoNumbers(5, 3);
+		System.out.println(c);
+
+	}
+	
+	public static int addTwoNumbers(int a, int b)
+	{
+		
+		return a+b;
+	}
+
+}
